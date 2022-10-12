9d5c424e93413633bf25015c9a933428de39ccc1
Ian Kersz <kerszamaral@gmail.com>
2022-10-12 15:59:32 +0000

    Updated template

--- commands/template.py
+++ commands/template.py
@@ -1,3 +1,4 @@
+# Para comandos agrupos em subgrupos, utilize essa template e chame add_command no main.py
 import discord
 from discord import app_commands as apc
 
@@ -11,3 +12,11 @@ class PetTEMPLATE(apc.Group):
     async def COMMANDNAME(self, interaction: discord.Integration):
         # YOUR CODE HERE
         pass
+
+# Para comandos na raiva do projeto, utilize essa template e simplesmente importe o arquivo no main.py
+import discord
+from utils.setup import CommandTree, TOKENS 
+
+@CommandTree.command(name="test", description="Teste de comando", guild = TOKENS.GUILD)
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message("Teste")
