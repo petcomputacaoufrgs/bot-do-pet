86248493b3b42c56e701950928cc4c475e774ded
Ian Kersz <kerszamaral@gmail.com>
2022-10-17 18:44:04 +0000

    Buttons are now working!

--- commands/key.py
+++ commands/key.py
@@ -2,18 +2,20 @@ import os
 import discord
 from discord import app_commands as apc
 from discord.ext import tasks
-from datetime import datetime, time
+from datetime import time
 from pytz import timezone
 import utils.buttons as btn
+from utils.env import update_env
 
 class Petkey(apc.Group): # Cria a classe do comando, que herda de Group, utilizado para agrupar os comandos em subgrupos
     def __init__(self, bot: discord.Client):
         super().__init__()
         self.bot = bot # Referencia para o proprio bot, caso necessario
+        self.keyMessageID = int(os.getenv("KEY_MESSAGE"))# ID da mensagem que contem a chave
     
     def check_rules(self, message):
         # Deleta todas as mensagens que não são a mais recente e não estão pinned
-        return not (message.id == self.message) and (message.pinned == False)
+        return not (message.id == self.keyMessageID) and (message.pinned == False)
     
     @apc.command(name="clear", description="Limpa o canal da chave") # Cria o comando /petkey clear
     async def clear(self, interaction: discord.Interaction): # Cria a função do comando
@@ -21,14 +23,32 @@ class Petkey(apc.Group): # Cria a classe do comando, que herda de Group, utiliza
         channel = self.bot.get_channel(int(os.getenv("KEY_CHANNEL"))) # Pega o canal da chave
         await channel.purge(check=self.check_rules) # Limpa o canal
         
+    @apc.command(name="chave", description="Gera a menssagem para o bot criar os botões") # Cria o comando /petkey chave
+    async def createKey(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Gerando a mensagem da chave...") # Responde ao comando
+        try:
+            await self.bot.get_channel(int(os.getenv("KEY_CHANNEL"))).get_partial_message(self.keyMessageID).edit(content="Mensagem atualizada!", embed=None, view=None)
+            await self.view.stop() # Para a task de atualização da chave
+        except:
+            pass
+        channel = self.bot.get_channel(interaction.channel_id)
+        self.keyMessageID = channel.last_message_id
+        update_env("KEY_CHANNEL", f"{interaction.channel_id}")
+        update_env("KEY_MESSAGE", f"{self.keyMessageID}")
+        self.key.restart() # Inicia o loop de atualização da chave
+        
     @tasks.loop(count=1)
     async def key(self):
+        channel = self.bot.get_channel(int(os.getenv("KEY_CHANNEL")))
+        try:
+            message = await channel.fetch_message(self.keyMessageID)
+        except:
+            return
+        
         self.view = btn.KeyMenu(self.bot)
         em = self.view.MsgChave()
-        channel = self.bot.get_channel(int(os.getenv("KEY_CHANNEL")))
-        await channel.send(embed=em, view=self.view)
-        self.message = channel.last_message_id
-        await channel.purge(check=self.check_rules)
+        await message.edit(content="", embed=em, view=self.view)
+        await self.view.wait()
 
     # Loop para avisar da chave esquecida
     @tasks.loop(time=time(hour=17, minute=54, tzinfo=timezone('America/Sao_Paulo'))) # Por algum motivo, se colocamos timezone ele só roda o comando 6 minutos depois
@@ -36,6 +56,14 @@ class Petkey(apc.Group): # Cria a classe do comando, que herda de Group, utiliza
         if self.view.location != 0:
             channel = self.bot.get_channel(int(os.getenv("KEY_CHANNEL")))
             await channel.send(f"<@{self.view.location }> vai levar a chave para casa hoje?", delete_after=60*60*4) # Manda a mensagem avisando que a chave está com alguem
+    
+    @tasks.loop(time=time(hour=23, minute=54, tzinfo=timezone('America/Sao_Paulo')))
+    async def updateNames(self):
+        try:
+            self.view.stop()
+        except:
+            pass
+        self.key.restart()
     
     @tasks.loop(count=1)
     async def startTasks(self):
--- utils/buttons.py
+++ utils/buttons.py
@@ -5,13 +5,14 @@ users = []
 
 class KeyMenu(discord.ui.View):
     def __init__(self, bot: discord.Client):
-        super().__init__()
+        super().__init__(timeout=None)
         self.location = 0  # Id da pessoa que está atualmente com a chave, 0 = com a tia
         self.bot = bot  # Referencia para o proprio bot, caso necessario
         self.updateUsers()
     
     def updateUsers(self):
         server = self.bot.get_guild(int(os.getenv("SERVER_ID")))
+        users.clear()
         for user in server.members:
             users.append(discord.SelectOption(
                 label=f"{user.display_name}", value=user.id, description=f"{user.name}#{user.discriminator}"))
@@ -26,19 +27,19 @@ class KeyMenu(discord.ui.View):
         # Manda a mensagem
         return em
         
-    @discord.ui.button(label="Peguei", style=discord.ButtonStyle.green)
+    @discord.ui.button(label="Peguei", style=discord.ButtonStyle.green, custom_id="peguei")
     async def peguei(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.location = interaction.user.id  # Muda o id da pessoa que está com a chave
         em = self.MsgChave()  # Gera a mensagem de saida
         await interaction.response.edit_message(embed=em)  # Manda a mensagem
     
-    @discord.ui.select(placeholder="Passei",options=users)
+    @discord.ui.select(placeholder="Passei",options=users, custom_id="passei")
     async def passei(self, interaction: discord.Interaction, select: discord.ui.Select):
         self.location = select.values[0]  # Muda o id da pessoa que está com a chave
         em = self.MsgChave()  # Gera a mensagem de saida
         await interaction.response.edit_message(embed=em)  # Manda a mensagem
         
-    @discord.ui.button(label="Devolvi", style=discord.ButtonStyle.red)
+    @discord.ui.button(label="Devolvi", style=discord.ButtonStyle.red, custom_id="devolvi")
     async def devolvi(self, interaction: discord.Interaction, button: discord.ui.Button):
         self.location = 0
         em = self.MsgChave()  # Gera a mensagem de saida
