bcff471038a0bd2613d06760c67144f980718f80
Ian Kersz <kerszamaral@gmail.com>
2023-01-06 19:20:04 -0300

    Added interpet group names to message

--- commands/interpet.py
+++ commands/interpet.py
@@ -18,13 +18,16 @@ class Petinter(apc.Group):
     @apc.command(name="interpet", description="Informa o dia do próximo interpet")
     async def interpet(self, interaction: discord.Interaction): 
         em = discord.Embed(color=0x9370DB) # Cria um embed
+        data = self.readInterpetFile() # Lê o arquivo de datas
         self.interpet_day = self.getNextInterpet().date() # Pega a data de hoje
         days_to_interpet = self.interpet_day - datetime.date.today() # Calcula a diferença entre a data de hoje e a data do interpet
         if days_to_interpet.days < 2: # Se a diferença for menor que 2 dias
             if days_to_interpet.days == 1: # Se a diferença for 1 dia
+                date = f'{self.interpet_day.day:02d}/{self.interpet_day.month:02d}/{self.interpet_day.year}'
                 em.add_field(
                     name="**Interpet**",
-                    value=f'Falta {days_to_interpet.days} dia até o próximo interpet, que será no dia {self.interpet_day.day:02d}/{self.interpet_day.month:02d}.'
+                    value=f'Falta {days_to_interpet.days} dia até o próximo interpet, que será no dia {self.interpet_day.day:02d}/{self.interpet_day.month:02d}.' + \
+                    f" Os grupos do interpet serão: {data[date]}."
                 )
             elif days_to_interpet.days == 0: # Se a diferença for 0 dias
                 em.add_field(
@@ -37,9 +40,11 @@ class Petinter(apc.Group):
                     value="Erro na data do interpet"
                 )
         else:
+            date = f'{self.interpet_day.day:02d}/{self.interpet_day.month:02d}/{self.interpet_day.year}'
             em.add_field( # Se a diferença for maior que 2 dias
                 name="**Interpet**",
-                value=f'Faltam {days_to_interpet.days} dias até o próximo interpet, que será no dia {self.interpet_day.day:02d}/{self.interpet_day.month:02d}.'
+                value=f'Faltam {days_to_interpet.days} dias até o próximo interpet, que será no dia {self.interpet_day.day:02d}/{self.interpet_day.month:02d}.' + \
+                f" Os grupos do interpet serão: {data[date]}."
             )
         await interaction.response.send_message(embed=em) # Envia a mensagem
         
@@ -64,25 +69,23 @@ class Petinter(apc.Group):
         await interaction.response.send_message(embed=em) # Envia a mensagem
         
     @apc.command(name="adicionar", description="Adiciona um novo interpet")
-    async def add_interpet(self, interaction: discord.Interaction, dia: int, mes: int, ano: int):
+    async def add_interpet(self, interaction: discord.Interaction, dia: int, mes: int, ano: int, grupos: str):
         data = self.readInterpetFile() # Lê o arquivo de datas
-        date_list = data['dates'] # Pega a lista de datas
         em = discord.Embed(color=0x9370DB) # Cria um embed
         try: # Tenta adicionar a data
             new_date = datetime.datetime(
                 int(ano), int(mes), int(dia)).date()
-            today_date = datetime.date.today()
-            if (new_date - today_date).days > 0:
-                if f'{dia:02d}/{mes:02d}/{ano}' in date_list:
+            if (new_date - datetime.date.today()).days > 0:
+                if f'{dia:02d}/{mes:02d}/{ano}' in data.keys():
                     em.add_field(
                         name="**Adicionar data de interpet**",
                         value="Essa data já está na lista."
                     )
                 else:
-                    date_list.append(f'{dia:02d}/{mes:02d}/{ano}')
-                    dict = {'dates': date_list}
+                    data[f'{dia:02d}/{mes:02d}/{ano}'] = grupos
+                    data = self.sortDates(data)
                     with open('data/interpet_dates.json', 'w+', encoding='utf-8') as outfile:
-                        json.dump(dict, outfile)
+                        json.dump(data, outfile)
                     em.add_field(
                         name="**Adicionar data de interpet**",
                         value=f'A data {dia:02d}/{mes:02d}/{ano} foi adicionada com sucesso!'
@@ -100,13 +103,11 @@ class Petinter(apc.Group):
     @apc.command(name="remover", description="Remove uma data de interpet")
     async def remove_interpet(self, interaction: discord.Interaction, dia: int, mes: int, ano: int):
         data = self.readInterpetFile()
-        date_list = data['dates']
         em = discord.Embed(color=0x9370DB)
-        if f'{dia:02d}/{mes:02d}/{ano}' in date_list:
-            date_list.remove(f'{dia:02d}/{mes:02d}/{ano}')
-            dict = {'dates': date_list}
+        if f'{dia:02d}/{mes:02d}/{ano}' in data.keys():
+            data.pop(f'{dia:02d}/{mes:02d}/{ano}')
             with open('data/interpet_dates.json', 'w+', encoding='utf-8') as outfile:
-                json.dump(dict, outfile)
+                json.dump(data, outfile)
             em.add_field(
                 name="**Remover data de interpet**",
                 value=f'A data foi removida da lista!'
@@ -120,17 +121,13 @@ class Petinter(apc.Group):
         
     @apc.command(name="datas", description="Mostra as datas de interpet")
     async def show_dates(self, interaction: discord.Interaction):
+        self.clearInterpetDates()
         data = self.readInterpetFile()
-        date_list = data['dates']
-        for date in date_list:
-            day, month, year = date.split('/')
-            difference = datetime.datetime(int(year), int(
-                month), int(day)).date() - datetime.date.today()
-            if (difference).days < 0:
-                date_list.remove(date)
-            else:
-                date = f"{date} - daqui a {(difference).days}"
-        printable_date_list = '\n'.join(date_list)
+
+        printable_date_list = ''
+        for date in data.keys():
+            printable_date_list += f'**{date}** - {data[date]}\n'
+
         em = discord.Embed(color=0x9370DB)
         em.add_field(
             name="**Datas dos próximos interpets:**",
@@ -162,13 +159,11 @@ class Petinter(apc.Group):
         self.awake_interpet.start() # Inicia a task de acordar para o interpet
         
     def getNextInterpet(self):
-        with open('data/interpet_dates.json', 'r', encoding='utf-8') as json_file:
-            data = json.load(json_file)
-            dates = data['dates']
+        data = self.readInterpetFile()
         now = datetime.datetime.now()
         actual_date = datetime.datetime(2022, 4, 9)
-        for i in dates:
-            day, month, year = i.split('/')
+        for date in data.keys():
+            day, month, year = date.split('/')
             formated_date = datetime.datetime(int(year), int(month), int(day))
             difference = formated_date - now
             if (actual_date - now).days < 0:
@@ -181,3 +176,34 @@ class Petinter(apc.Group):
         with open('data/interpet_dates.json', 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
         return data
+        
+    def clearInterpetDates(self):
+        data = self.readInterpetFile()
+        oldDates = []
+        for date in data.keys():
+            day, month, year = date.split('/')
+            difference = datetime.datetime(int(year), int(
+                month), int(day)).date() - datetime.date.today()
+            if (difference).days < 0:
+                oldDates.append(date)
+                
+        for date in oldDates:
+            data.pop(date)
+                
+        with open('data/interpet_dates.json', 'w+', encoding='utf-8') as outfile:
+                json.dump(data, outfile)
+        
+    def sortDates(self, data: dict) -> dict:
+        sorted_data = {}
+        # Ordena o dicionario
+        dates = []
+        for date in data.keys():
+            day, month, year = date.split('/')
+            dates.append(datetime.datetime(int(year), int(month), int(day)))
+            
+        dates.sort()
+        
+        for date in dates:
+            sorted_data[f'{date.day:02d}/{date.month:02d}/{date.year}'] = data[f'{date.day:02d}/{date.month:02d}/{date.year}']
+
+        return sorted_data
